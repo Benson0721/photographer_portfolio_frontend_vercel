5e66b8b933e1bab938485823497ca247bca9aaff
Mpass <121707487+ME942082@users.noreply.github.com>
2025-05-28 23:38:08 +0800

    修正email+拖移功能bug

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,6 +4,8 @@ const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 const EMAILJS_USER_ID = import.meta.env.VITE_EMAILJS_USER_ID;
 
+console.log(import.meta.env);
+
 export const sendEmail = async (data) => {
   try {
     await emailjs.send(
